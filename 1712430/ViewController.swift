25a469e78bca92e00f5f0abbd4d30df02337bcc6
Van Hiep <levanhiep.95.95@gmail.com>
2019-11-03 11:22:01 +0700

    Initial Commit

---
+++ 1712430/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  1712430
+//
+//  Created by I Am Focused on 11/3/19.
+//  Copyright © 2019 LVHhcmus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 1712430Tests/_712430Tests.swift
@@ -0,0 +1,34 @@
+//
+//  _712430Tests.swift
+//  1712430Tests
+//
+//  Created by I Am Focused on 11/3/19.
+//  Copyright © 2019 LVHhcmus. All rights reserved.
+//
+
+import XCTest
+@testable import _712430
+
+class _712430Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
